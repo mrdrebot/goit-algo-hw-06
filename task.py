25bb90f25f52728bf2389ea02af01d725328dfc2
mrdrebot <m.r.drebot.j@gmail.com>
2024-10-30 17:20:05 +0200

    Added new methods in class AddressBook(), Record()

--- task.py
+++ task.py
@@ -26,7 +26,6 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    # реалізація класу
     def add_phone(self, phone_number):
         new_phone = Phone(phone_number)
         self.phones.append(new_phone)
@@ -37,14 +36,19 @@ class Record:
                 self.phones.remove(phone)
 
     def edit_phone(self, old_phone, new_phone):
+        old_p = Phone(old_phone)
+        new_p = Phone(new_phone)
+
         for phone in self.phones:
-            if phone.value == old_phone:
-                phone.value = new_phone
+            if phone.value == old_p.value:
+                phone.value = new_p.value
 
     def search_phone(self, phone_number):
         for phone in self.phones:
             if phone.value == phone_number:
                 return phone
+            
+        return None
 
 
     def __str__(self):
@@ -53,31 +57,47 @@ class Record:
 class AddressBook(UserDict):
     # реалізація класу
     # def __init__(self, user_record):
+    #     print(user_record)
     #     self.user_record = user_record
 
-    def add_record(self, user_record: dict) -> None:
-          print(user_record)
-        #   pass
-		
-# name = Name(11111)
-# print(name.value)
-# print(type(name.value))
+    def add_record(self, user_record):
+        #   print(user_record)
+        #   print(user_record.name)
+        #   print(user_record.phones)
+
+          self.data[user_record.name.value] = user_record
+
+    def search_contact(self, contact_name):
+        return self.data.get(contact_name)
+
+    def __str__(self):
+        # return str(self.data)
+        return f"Contacts:\n{'\n'.join(self.data.keys())}"
 
 book = AddressBook()
 # print(book)
 maks_record = Record("Maks")
+miha_record = Record("Miha")
+alex_record = Record("Alex")
 # maks_record = Record("")
-print(maks_record)
+# print(maks_record)
 maks_record.add_phone("1234567890")
 maks_record.add_phone("5555555555")
 maks_record.add_phone("5555555556")
-print(maks_record)
+miha_record.add_phone("5555555556")
+alex_record.add_phone("5555555556")
+# print(maks_record)
 maks_record.remove_phone("5555555556")
-print(maks_record)
+# print(maks_record)
 maks_record.edit_phone("5555555555", "7777777777")
-print(maks_record)
-print(maks_record.search_phone("7777777777"))
+# print(maks_record)
+# print(maks_record.search_phone("7777777777"))
 print(maks_record.search_phone("7777777779"))
 
 book.add_record(maks_record)
+book.add_record(miha_record)
+book.add_record(alex_record)
 print(book)
+
+print(book.search_contact("Maks"))
+print(book.search_contact("Maks1"))
